d4c32d1ba0fb3148de8b7f8d50e6686115f9c6b8
KarenSarmiento <karen.sarmiento25@gmail.com>
2019-11-18 17:29:30 +0000

    Small refactor to use kotlin-like feature.

--- app/src/main/java/com/karensarmiento/collaborationapp/JsonMessageExtension.kt
+++ app/src/main/java/com/karensarmiento/collaborationapp/JsonMessageExtension.kt
@@ -7,18 +7,13 @@ import org.jivesoftware.smack.packet.Message
 
 /**
  * This extension enables you to send JSONs in a Smack Message (Packet).
+ * An extension element is an XML subdocument with a root element name and namespace.
  */
 class JsonMessageExtension(val json: String) : ExtensionElement {
     override fun toXML(): CharSequence {
         // TODO: 1. Do we need to escape the JSON? - StringUtils.escapeForXML(json)
         // 2. How do we use the enclosing namespace?
-        return String.format(
-            "<%s xmlns=\"%s\">%s</%s>",
-            elementName,
-            namespace,
-            json,
-            elementName
-        )
+        return "<$elementName xmlns=\"$namespace\">$json</$elementName>"
     }
 
     fun toPacket(): Stanza {
@@ -27,10 +22,12 @@ class JsonMessageExtension(val json: String) : ExtensionElement {
         return message
     }
 
+    // Returns root element name.
     override fun getElementName(): String {
         return Utils.FCM_ELEMENT_NAME
     }
 
+    // Returns the root element name xml namespace.
     override fun getNamespace(): String {
         return Utils.FCM_NAMESPACE
     }
