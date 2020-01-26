1ba443ee92b9d0767dc33058838c8ba161473bf8
KarenSarmiento <karen.sarmiento25@gmail.com>
2020-01-26 18:45:20 +0000

    Encrypt all outgoing messages.
    This includes: device group messages (json updates) and create group requests.

--- app/src/main/java/com/karensarmiento/collaborationapp/messaging/FirebaseMessageSendingService.kt
+++ app/src/main/java/com/karensarmiento/collaborationapp/messaging/FirebaseMessageSendingService.kt
@@ -17,18 +17,63 @@ object FirebaseMessageSendingService {
     private const val TAG = "FirebaseSendingService"
     private const val TTL = 2000
 
-    // TODO: Add comments containing JSON example for each of these methods.
     fun sendRegisterPublicKeyRequest(publicKey: String) {
-        // Crate request.
+        // Create request.
         val request = Json.createObjectBuilder()
             .add(Jk.UPSTREAM_TYPE.text, Jk.REGISTER_PUBLIC_KEY.text)
             .add(Jk.EMAIL.text, Utils.getGoogleEmail())
-            .add(Jk.PUBLIC_KEY.text, publicKey).build().toString()
+            .add(Jk.PUBLIC_KEY.text, publicKey)
+            .build().toString()
 
         // Send request to server.
         val messageId = Utils.getUniqueId()
         sendEncryptedServerRequest(request, messageId)
         Log.i(TAG, "Sent register public key request to server: $messageId")
+    }
+
+    fun sendJsonUpdateToCurrentDeviceGroup(jsonUpdate: String) {
+        val currentGroup = GroupManager.currentGroup
+        if (currentGroup == null) {
+            Log.e(TAG, "Could not send message to current group since it is null.")
+            return
+        }
+        sendJsonUpdateToDeviceGroup(currentGroup, jsonUpdate)
+    }
+
+    private fun sendJsonUpdateToDeviceGroup(groupName: String, jsonUpdate: String) {
+        // Create request.
+        val groupToken = GroupManager.groupId(groupName)
+        val request = Json.createObjectBuilder()
+            .add(Jk.UPSTREAM_TYPE.text, Jk.FORWARD_MESSAGE.text)
+            .add(Jk.FORWARD_TOKEN_ID.text, groupToken)
+            .add(Jk.JSON_UPDATE.text, jsonUpdate)
+            .build().toString()
+
+        // Send request to server.
+        val messageId = Utils.getUniqueId()
+        sendEncryptedServerRequest(request, messageId)
+        Log.i(TAG, "Sent message to device group: $messageId")
+    }
+
+    fun sendCreateGroupRequest(groupName: String, peerEmail: String) {
+        // Create request.
+        val groupId = Utils.getUniqueId()
+        val memberEmails = JSONArray()
+        memberEmails.put(peerEmail)
+        val request = Json.createObjectBuilder()
+            .add(Jk.UPSTREAM_TYPE.text, Jk.CREATE_GROUP.text)
+            .add(Jk.GROUP_NAME.text, groupName)
+            .add(Jk.GROUP_ID.text, groupId)
+            .add(Jk.MEMBER_EMAILS.text, memberEmails.toString())
+            .build().toString()
+
+        // Register this request as one that is awaiting a response.
+        val messageId = Utils.getUniqueId()
+        MessageBuffer.registerWaitingRequest(messageId)
+
+        // Send request to server.
+        sendEncryptedServerRequest(request, messageId)
+        Log.i(TAG, "Sent create group request to server: $messageId")
     }
 
     private fun sendEncryptedServerRequest(request: String, messageId: String) {
@@ -50,52 +95,5 @@ object FirebaseMessageSendingService {
                 .addData(Jk.ENC_MESSAGE.text, encryptedRequest)
                 .addData(Jk.ENC_KEY.text, encryptedKey)
                 .build())
-    }
-
-    fun sendJsonUpdateToCurrentDeviceGroup(jsonUpdate: String) {
-        val currentGroup = GroupManager.currentGroup
-        if (currentGroup == null) {
-            Log.e(TAG, "Could not send message to current group since it is null.")
-            return
-        }
-        sendJsonUpdateToDeviceGroup(currentGroup, jsonUpdate)
-    }
-
-    private fun sendJsonUpdateToDeviceGroup(groupName: String, jsonUpdate: String) {
-        val messageId = Utils.getUniqueId()
-        val groupToken = GroupManager.groupId(groupName)
-
-        FirebaseMessaging.getInstance().send(
-            RemoteMessage.Builder("${Utils.SENDER_ID}@fcm.googleapis.com")
-                .setMessageId(messageId)
-                .setTtl(TTL)
-                .addData(Jk.UPSTREAM_TYPE.text, Jk.FORWARD_MESSAGE.text)
-                .addData(Jk.FORWARD_TOKEN_ID.text, groupToken)
-                .addData(Jk.JSON_UPDATE.text, jsonUpdate)
-                .build())
-        Log.i(TAG, "Sent message to device group: $messageId")
-    }
-
-    fun sendCreateGroupRequest(groupName: String, peerEmail: String) {
-        val messageId = Utils.getUniqueId()
-        val groupId = Utils.getUniqueId()
-
-        // Register this request as one that is awaiting a response.
-        MessageBuffer.registerWaitingRequest(messageId)
-
-        // Send request
-        val memberEmails = JSONArray()
-        memberEmails.put(peerEmail)
-        FirebaseMessaging.getInstance().send(
-            RemoteMessage.Builder("${Utils.SENDER_ID}@fcm.googleapis.com")
-                .setMessageId(messageId)
-                .setTtl(TTL)
-                .addData(Jk.UPSTREAM_TYPE.text, Jk.CREATE_GROUP.text)
-                .addData(Jk.GROUP_NAME.text, groupName)
-                .addData(Jk.GROUP_ID.text, groupId)
-                .addData(Jk.MEMBER_EMAILS.text, memberEmails.toString())
-                .build()
-        )
-        Log.i(TAG, "Sent create group request to server: $messageId")
     }
 }
