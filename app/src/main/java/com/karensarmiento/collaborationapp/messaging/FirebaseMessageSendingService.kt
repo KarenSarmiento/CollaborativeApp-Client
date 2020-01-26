0db5dc7713519b39f7f68b0697862b7572149d23
KarenSarmiento <karen.sarmiento25@gmail.com>
2020-01-26 18:34:23 +0000

    Fix indent.

--- app/src/main/java/com/karensarmiento/collaborationapp/messaging/FirebaseMessageSendingService.kt
+++ app/src/main/java/com/karensarmiento/collaborationapp/messaging/FirebaseMessageSendingService.kt
@@ -62,40 +62,40 @@ object FirebaseMessageSendingService {
     }
 
     private fun sendJsonUpdateToDeviceGroup(groupName: String, jsonUpdate: String) {
-            val messageId = Utils.getUniqueId()
-            val groupToken = GroupManager.groupId(groupName)
+        val messageId = Utils.getUniqueId()
+        val groupToken = GroupManager.groupId(groupName)
 
-            FirebaseMessaging.getInstance().send(
-                RemoteMessage.Builder("${Utils.SENDER_ID}@fcm.googleapis.com")
-                    .setMessageId(messageId)
-                    .setTtl(TTL)
-                    .addData(Jk.UPSTREAM_TYPE.text, Jk.FORWARD_MESSAGE.text)
-                    .addData(Jk.FORWARD_TOKEN_ID.text, groupToken)
-                    .addData(Jk.JSON_UPDATE.text, jsonUpdate)
-                    .build())
-            Log.i(TAG, "Sent message to device group: $messageId")
-        }
-
-        fun sendCreateGroupRequest(groupName: String, peerEmail: String) {
-            val messageId = Utils.getUniqueId()
-            val groupId = Utils.getUniqueId()
-
-            // Register this request as one that is awaiting a response.
-            MessageBuffer.registerWaitingRequest(messageId)
-
-            // Send request
-            val memberEmails = JSONArray()
-            memberEmails.put(peerEmail)
-            FirebaseMessaging.getInstance().send(
-                RemoteMessage.Builder("${Utils.SENDER_ID}@fcm.googleapis.com")
-                    .setMessageId(messageId)
-                    .setTtl(TTL)
-                    .addData(Jk.UPSTREAM_TYPE.text, Jk.CREATE_GROUP.text)
-                    .addData(Jk.GROUP_NAME.text, groupName)
-                    .addData(Jk.GROUP_ID.text, groupId)
-                    .addData(Jk.MEMBER_EMAILS.text, memberEmails.toString())
-                    .build()
-            )
-            Log.i(TAG, "Sent create group request to server: $messageId")
-        }
+        FirebaseMessaging.getInstance().send(
+            RemoteMessage.Builder("${Utils.SENDER_ID}@fcm.googleapis.com")
+                .setMessageId(messageId)
+                .setTtl(TTL)
+                .addData(Jk.UPSTREAM_TYPE.text, Jk.FORWARD_MESSAGE.text)
+                .addData(Jk.FORWARD_TOKEN_ID.text, groupToken)
+                .addData(Jk.JSON_UPDATE.text, jsonUpdate)
+                .build())
+        Log.i(TAG, "Sent message to device group: $messageId")
     }
+
+    fun sendCreateGroupRequest(groupName: String, peerEmail: String) {
+        val messageId = Utils.getUniqueId()
+        val groupId = Utils.getUniqueId()
+
+        // Register this request as one that is awaiting a response.
+        MessageBuffer.registerWaitingRequest(messageId)
+
+        // Send request
+        val memberEmails = JSONArray()
+        memberEmails.put(peerEmail)
+        FirebaseMessaging.getInstance().send(
+            RemoteMessage.Builder("${Utils.SENDER_ID}@fcm.googleapis.com")
+                .setMessageId(messageId)
+                .setTtl(TTL)
+                .addData(Jk.UPSTREAM_TYPE.text, Jk.CREATE_GROUP.text)
+                .addData(Jk.GROUP_NAME.text, groupName)
+                .addData(Jk.GROUP_ID.text, groupId)
+                .addData(Jk.MEMBER_EMAILS.text, memberEmails.toString())
+                .build()
+        )
+        Log.i(TAG, "Sent create group request to server: $messageId")
+    }
+}
